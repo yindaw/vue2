dc458e31c1433cd9de44b6a976f4c47e9742d924
yindaw <2857705371@qq.com>
2021-07-18 00:37:41 +0800

    04

---
+++ 课堂代码/my-site/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>App组件</h1>
+    <Avatar
+      url="https://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar8.jpg"
+      :size="100"
+    />
+    <Avatar
+      url="http://mdrs.yuanjin.tech/FgMwAPYq17So9nwVH44ltDHo7u3c"
+      :size="50"
+    />
+  </div>
+</template>
+<script>
+import Avatar from "./components/Avatar.vue";
+export default {
+  components: {
+    name: "App",
+    Avatar,
+  },
+};
+</script>
