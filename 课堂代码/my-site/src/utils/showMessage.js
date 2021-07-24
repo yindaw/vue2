737fc54c767df6d967b4d34d62ec78e6e806f8fe
yindaw <2857705371@qq.com>
2021-07-24 18:22:10 +0800

    12

--- 课堂代码/my-site/src/utils/showMessage.js
+++ 课堂代码/my-site/src/utils/showMessage.js
@@ -1,12 +1,13 @@
 import getComponentRootDom from "./getComponentRootDom";
 import Icon from "@/components/Icon";
 import styles from "./showMessage.module.less";
+
 /**
- * 
+ * 弹出消息
  * @param {String} content 消息内容
- * @param {String} type 消息类型 error info success warn
+ * @param {String} type 消息类型  info  error  success  warn
  * @param {Number} duration 多久后消失
- * @param {HTMLElement} container 容器, 消息会显示到该容器的正中;如果不传，则显示到页面正中
+ * @param {HTMLElement} container 容器，消息会显示到该容器的正中；如果不传，则显示到页面正中
  */
 export default function (options = {}) {
   const content = options.content || "";
@@ -20,31 +21,38 @@ export default function (options = {}) {
   });
   div.innerHTML = `<span class="${styles.icon}">${iconDom.outerHTML}</span><div>${content}</div>`;
   // 设置样式
-  const typeClassName = styles[`message-${type}`]; // 类型样式名
+  const typeClassName = styles[`message-${type}`]; //类型样式名
   div.className = `${styles.message} ${typeClassName}`;
   // 将div加入到容器中
+
   // 容器的position是否改动过
-  if (getComputedStyle(container).position === "static") {
-    container.style.position = "relative";
+  if (options.container) {
+    if (getComputedStyle(container).position === "static") {
+      container.style.position = "relative";
+    }
   }
 
   container.appendChild(div);
   // 浏览器强行渲染
   div.clientHeight; // 导致reflow
+
   // 回归到正常位置
   div.style.opacity = 1;
   div.style.transform = `translate(-50%, -50%)`;
 
-  // 等待一段时间, 消失
+  // 等一段时间，消失
   setTimeout(() => {
     div.style.opacity = 0;
-    div.style.transform = `translate(-50%, -50%) translateY(-15px)`;
-    div.addEventListener("transitionend", function () {
-      div.remove();
-      // 运行回调函数
-      options.callback && options.callback();
-    }, {
-      once: true
-    });
+    div.style.transform = `translate(-50%, -50%) translateY(-25px)`;
+    div.addEventListener(
+      "transitionend",
+      function () {
+        div.remove();
+        // 运行回调函数
+        options.callback && options.callback();
+      }, {
+        once: true
+      }
+    );
   }, duration);
 }
