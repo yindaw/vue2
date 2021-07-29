64512f7e21c5f7d2798544bb173d4c6e919fa00e
yindaw <2857705371@qq.com>
2021-07-29 21:54:28 +0800

    14

---
+++ 课堂代码/my-site/src/views/Home/CarouselItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="carousel-item-container">CarouselItem</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+@import "~@/styles/var.less"; 
+.carousel-item-container {
+  background: @dark;
+  height: 100%;
+  width: 100%;
+  color:#fff;
+}
+</style>
--- 课堂代码/my-site/src/views/Home/index.vue
+++ 课堂代码/my-site/src/views/Home/index.vue
@@ -1,25 +1,142 @@
 <template>
-  <div ref="container" v-if="banners.length > 0">
-    <ul>
+  <div class="home-container" ref="container">
+    <ul class="carousel-container" :style="{ marginTop }">
       <li v-for="item in banners" :key="item.id">
-        <img :src="item.midImg" alt="" />
-        <h2>{{ item.title }}</h2>
-        <h3>{{ item.description }}</h3>
+        <CarouselItem />
       </li>
+    </ul>
+    <div v-show="index >= 1" class="icon icon-up" @click="switchTo(index - 1)">
+      <Icon type="arrowUp" />
+    </div>
+    <div v-show="index < banners.length - 1" class="icon icon-down" @click="switchTo(index + 1)">
+      <Icon type="arrowDown" />
+    </div>
+    <ul class="indicator">
+      <li
+        :class="{ active: i === index }"
+        v-for="(item, i) in banners"
+        :key="item.id"
+        @click="switchTo(i)"
+      ></li>
     </ul>
   </div>
 </template>
 
 <script>
 import { getBanners } from "@/api/banner";
+import CarouselItem from "./CarouselItem";
+import Icon from "@/components/Icon";
 export default {
+  components: {
+    CarouselItem,
+    Icon,
+  },
   data() {
     return {
       banners: [],
+      index: 0, // 当前显示的是第几张轮播图
+      containerHeight: 0, // 整个容器的高度
     };
   },
   async created() {
     this.banners = await getBanners();
   },
+  mounted() {
+    this.containerHeight = this.$refs.container.clientHeight;
+  },
+  computed: {
+    marginTop() {
+      return -this.index * this.containerHeight + "px";
+    },
+  },
+  methods: {
+    // 切换轮播图
+    switchTo(i) {
+      this.index = i;
+    }
+  },
 };
 </script> 
+<style scoped lang="less">
+@import "~@/styles/mixin.less";
+@import "~@/styles/var.less";
+.home-container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+}
+.carousel-container {
+  width: 100%;
+  height: 100%;
+  transition: 500ms;
+  li {
+    width: 100%;
+    height: 100%;
+  }
+}
+.icon {
+  .self-center();
+  font-size: 30px;
+  @gap: 25px;
+  color: @gray;
+  cursor: pointer;
+  transform: translateX(-50%);
+  &.icon-up {
+    top: @gap;
+    animation: jump-up 2s infinite;
+  }
+  &.icon-down {
+    top: auto;
+    bottom: @gap;
+    animation: jump-down 2s infinite;
+  }
+  @jump: 5px;
+  @keyframes jump-up {
+    0% {
+      transform: translate(-50%, @jump);
+    }
+    50% {
+      transform: translate(-50%, -@jump);
+    }
+    100% {
+      transform: translate(-50%, @jump);
+    }
+  }
+  @keyframes jump-down {
+    0% {
+      transform: translate(-50%, -@jump);
+    }
+    50% {
+      transform: translate(-50%, @jump);
+    }
+    100% {
+      transform: translate(-50%, -@jump);
+    }
+  }
+}
+.indicator {
+  .self-center();
+  transform: translateY(-50%);
+  left: auto;
+  right: 20px;
+  li {
+    width: 7px;
+    height: 7px;
+    border-radius: 50%;
+    background: @words;
+    cursor: pointer;
+    margin-bottom: 10px;
+    border: 1px solid #fff;
+    box-sizing: border-box;
+    &.active {
+      background: #fff;
+    }
+  }
+}
+</style>
